40c3e4f5859f0d3c57e812711ac4e8c79c2051c2
Adeoye <abdulazeezbabatunde@gmail.com>
2023-09-18 15:27:22 -0700

    updated

--- functions1.c
+++ functions1.c
@@ -3,7 +3,6 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * @name: abdulazeez & kenji
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
